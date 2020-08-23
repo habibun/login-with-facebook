0b5b9ade131004af4af0a95cdca4fb9f7213b69a
Habibun Noby <habibun.noby@gmail.com>
2020-08-23 19:44:20 +0600

    Add facebook login using php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Facebook\Facebook;
+use function Symfony\Component\String\u;
 
 class SecurityController extends AbstractController
 {
@@ -14,6 +20,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        if (!session_id()) {
+            session_start();
+        }
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
         // }
@@ -23,7 +32,33 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        //facebook
+
+        $appId = $this->getParameter('fb.app_id');
+        $appSecret = $this->getParameter('fb.app_secret');
+        $fb = new Facebook([
+            'app_id' => $appId,
+            'app_secret' => $appSecret,
+            'default_graph_version' => 'v2.10',
+        ]);
+
+//        dump($this->getParameter('fb.app_id'));
+//        dump($this->getParameter('fb.app_secret'));
+//        exit();
+
+        $helper = $fb->getRedirectLoginHelper();
+
+        $permissions = ['email']; // Optional permissions
+        $fbLoginUrl = $helper->getLoginUrl('https://localhost:8000/fb-callback', $permissions);
+
+        return $this->render('security/login.html.twig',
+            [
+                'last_username' => $lastUsername,
+                'error' => $error,
+                'fb_login_url' => $fbLoginUrl,
+                'app_id' => $appId,
+                'app_secret' => $appSecret
+            ]);
     }
 
     /**
@@ -32,5 +67,67 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/fb-callback", name="fb_callback")
+     */
+    public function fbCallback()
+    {
+        if(!session_id()) {
+            session_start();
+        }
+
+        //facebook
+        $fb = new Facebook([
+            'app_id' => $this->getParameter('fb.app_id'),
+            'app_secret' => $this->getParameter('fb.app_secret'),
+            'default_graph_version' => 'v2.10',
+        ]);
+
+        $helper = $fb->getRedirectLoginHelper();
+
+        try {
+            $accessToken = $helper->getAccessToken();
+        } catch(FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch(FacebookSDKException $e) {
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+
+        if (! isset($accessToken)) {
+            if ($helper->getError()) {
+                header('HTTP/1.0 401 Unauthorized');
+                echo "Error: " . $helper->getError() . "\n";
+                echo "Error Code: " . $helper->getErrorCode() . "\n";
+                echo "Error Reason: " . $helper->getErrorReason() . "\n";
+                echo "Error Description: " . $helper->getErrorDescription() . "\n";
+            } else {
+                header('HTTP/1.0 400 Bad Request');
+                echo 'Bad request';
+            }
+            exit;
+        }
+
+        try {
+            // Returns a `Facebook\FacebookResponse` object
+            $response = $fb->get('/me?fields=id,name', $accessToken);
+        } catch(FacebookResponseException $e) {
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch(FacebookSDKException $e) {
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+
+        $user = $response->getGraphUser();
+
+        dump($user);
+
+        exit('getting callback');
     }
 }
